9156934e3618c59803365b16dc50d6f82f39edc7
rojin <therojinebrahimi@gmail.com>
2021-04-17 20:44:00 +0430

    Add try/except

--- ex01.py
+++ ex01.py
@@ -1,20 +1,23 @@
 from random import randint
 
 def guessing_game():
-    
     random_number = randint(0, 100)
     
-    while True: 
-        user_guess = int(input("\nGuess the number: "))
+    while True:
+        try: 
+            user_guess = int(input("\nGuess the number: "))
         
-        if user_guess > random_number:
-            print("Too high")
+            if user_guess > random_number:
+                print("Too high")
                  
-        elif user_guess < random_number:
-            print("Too low")
+            elif user_guess < random_number:
+                print("Too low")
 
-        elif user_guess == random_number:
-            print("Just right")
-            break  
-      
+            elif user_guess == random_number:
+                print("Just right")
+                break 
+             
+        except ValueError:
+            print("The entered data is not integer.")
+
 guessing_game() 
