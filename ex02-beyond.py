bd6dd9b203f07f8a8232dcce7028ba59ef56b764
rojin <therojinebrahimi@gmail.com>
2021-04-19 01:17:40 +0430

    Add word_length() function

--- ex02-beyond.py
+++ ex02-beyond.py
@@ -6,11 +6,15 @@ def mysum(*numbers, extra=0):
         
     return result + extra
 
-
 def average(*numbers):
     return mysum(*numbers) / len(numbers)
 
+def word_length(*words):
+    w = [ len(w) for w in words ]
+    return (max(w), int((max(w) + min(w)) / len(words)), min(w))
+ 
 
-print(mysum(1, 6, 219.42, .7, 9, 17, 0), 10)
-print(mysum(*[1, 6, 219.42, .7, 9, 17.89, 0.9], 10))
+print(mysum(1, 6, 219.42, .7, 9, 17, 0))
+print(mysum(*[1, 6, 219.42, .7, 9, 17.89, 0.9]))
 print(average(1, 6, 219.42, .7, 9, 17, 0))
+print(word_length("Love", "Coding", "This", "Way", "!"))
