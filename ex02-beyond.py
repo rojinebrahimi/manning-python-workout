20bd8f7c24f5c137eeb5eee3729158361e089d42
rojin <therojinebrahimi@gmail.com>
2021-04-19 01:05:45 +0430

    Add average() function

--- ex02-beyond.py
+++ ex02-beyond.py
@@ -7,5 +7,10 @@ def mysum(*numbers, extra=0):
     return result + extra
 
 
+def average(*numbers):
+    return mysum(*numbers) / len(numbers)
+
+
 print(mysum(1, 6, 219.42, .7, 9, 17, 0), 10)
 print(mysum(*[1, 6, 219.42, .7, 9, 17.89, 0.9], 10))
+print(average(1, 6, 219.42, .7, 9, 17, 0))
